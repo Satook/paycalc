0ff1fb183483584fd2dc108d8be4c0185f5d07c1
Andrew <me@satook.com>
2019-07-06 17:30:52 +1000

    Basic outline started

--- paycalc/cmd/csv.py
+++ paycalc/cmd/csv.py
@@ -1,7 +1,8 @@
 
 import argparse
+import csv
 import sys
-from paycalc import csvparse
+from paycalc import parse
 
 def do_paycalc():
     '''
@@ -23,12 +24,23 @@ Annual salary must be a number without commas or punctuation, e.g. 60050.
 Super rate should be a number from 0-100 followed by a % sign, e.g. 9%.
 Payment month should be a "month year" string, e.g. "jan 2018" or "March 2020".
 """)
-    parser.add_argument('--hasheader', action='store_true', help='The CSV data has a header row')
+    parser.add_argument('--skipfirst', action='store_true', help='Skip the first line, often a header')
 
     # we don't have any arguments, we just wanted a nice --help answer :)
     args = parser.parse_args()
 
-    src_rows = csvparse.parse_data(sys.stdin)
+    process_csv(sys.stdin, sys.stdout, args.skipfirst)
 
-    for r in src_rows:
-        result =
+ROW_PRINTERS = [
+    lambda fn: fn,
+    lambda ln: ln,
+]
+
+def process_csv(filein, fileout, skip_first_row):
+
+    writer = csv.writer(fileout)
+
+    for in_row in parse.parse_csv(sys.stdin):
+        out_row = process_slip(in_row)
+
+        writer.writerow(out_row)
